2ad47864e469bd26680dbeec9e3d3aa1bddab7e1
chris poirel <chris@redowlanalytics.com>
2015-12-04 21:40:36 -0500

    solved problem 20

---
+++ 020.py
@@ -0,0 +1,11 @@
+"""
+Project Euler Problem 20
+========================
+
+n! means n * (n - 1) * ... * 3 * 2 * 1
+
+Find the sum of the digits in the number 100!
+"""
+
+from math import factorial
+print sum([int(x) for x in str(factorial(100))])
